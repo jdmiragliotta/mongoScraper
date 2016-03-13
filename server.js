764c64b21da56f4ba4300944b3bad2c6e4ab9cc2
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-03-13 10:35:47 -0400

    Added connection to db and scraper code

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require("express")
+var mongoose = require("mongoose")
+var bodyParser = require("body-parser")
+var cheerio = require("cheerio")
+var request = require("request")
+var expressHandlebars = require("express-handlebars");
